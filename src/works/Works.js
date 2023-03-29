f0fab6e57b45618db3dc5ae06bf223b24704d440
rus55 <r-mit@mail.ru>
2023-03-29 23:05:01 +0300

    new commit

--- src/works/Works.js
+++ src/works/Works.js
@@ -4,27 +4,59 @@ import Work from "./work/Work";
 import Title from "../common/components/title/Title";
 import todoImage from "../assets/images/todolist.png";
 import socialImage from "../assets/images/social.png";
+import saunaImage from "../assets/images/sauna.png";
 
 function Works() {
-    const social = {
-        color: 'blue',
-        backgroundImage: `url(${socialImage})`,
-    };
-    const todolist = {
-        color: 'blue',
-        backgroundImage: `url(${todoImage})`,
-    };
+    const projects = [
+        {
+            title: 'Social Network',
+            logo: socialImage,
+            description: 'SPA for social action - messaging between users.',
+            linkToGHPage: 'https://github.com/rus55/samurai-way-main'
+        },
+        {
+            title: 'Todolist',
+            logo: todoImage,
+            description: 'SPA to create and further manage the to-do list and its items for the user.',
+            linkToGHPage: ''
+        },
+        {
+            title: 'Application for kindling the stove',
+            logo: saunaImage,
+            description: 'Educational game for kindling the stove in the sauna',
+            linkToGHPage: 'https://rus55.github.io/Educational-game/'
+        },
+        {
+            title: 'Personal blog',
+            logo: null,
+            description: 'Single page application "Personal blog". Frontend is written in React. The backend is implemented on Hapi using MongoDB.',
+            linkToGHPage: 'https://github.com/rus55/Personal-blog'
+        },
+        {
+            title: 'Search movie',
+            logo: null,
+            description: 'This project is written in ReactJS using Redux, hooks and modular styles.',
+            linkToGHPage: 'https://github.com/rus55/Search-movie'
+        },
+        {
+            title: 'Banner-editor',
+            logo: null,
+            description: 'Application - editor for creating banners on CANVAS.',
+            linkToGHPage: 'https://github.com/rus55/Banner-editor'
+        }
+    ]
     return (
         <div className={style.worksBlock} id='projects'>
             <div className={`${styleContainer.container} ${style.worksContainer}`}>
                 <Title text={'Projects'} />
                 <div className={style.works}>
-                    <Work style={social} title={'Social Network'} description={'Way of the Samurai'}/>
-                    <Work style={todolist} title={'Todolist'} description={'Development Blogddddddddddd dddddddddddddddd dddddddddddddd dddddddddddddd ddddddddddddddd dddddddddd'}/>
-                    <Work style={todolist} title={'Educational-game'} description={'Development Blogddddddddddd dddddddddddddddd dddddddddddddd dddddddddddddd ddddddddddddddd dddddddddd'}/>
-                    <Work style={todolist} title={'Personal-blog'} description={'Development Blogddddddddddd dddddddddddddddd dddddddddddddd dddddddddddddd ddddddddddddddd dddddddddd'}/>
-                    <Work style={todolist} title={'Search-movie'} description={'Development Blogddddddddddd dddddddddddddddd dddddddddddddd dddddddddddddd ddddddddddddddd dddddddddd'}/>
-                    <Work style={todolist} title={'Banner-editor '} description={'Development Blogddddddddddd dddddddddddddddd dddddddddddddd dddddddddddddd ddddddddddddddd dddddddddd'}/>
+                    {projects.map( (project, index) => <Work
+                        title={project.title}
+                        logo={project.logo}
+                        description={project.description}
+                        linkToGHPage={project.linkToGHPage}
+                        key={project + index}
+                    /> )}
                 </div>
             </div>
         </div>
--- src/works/work/Work.js
+++ src/works/work/Work.js
@@ -1,12 +1,15 @@
 import style from './Work.module.scss'
 
 function Work(props) {
-
     return (
         <div className={style.work}>
-            <div className={style.photo} style={props.style}>
-                {/*<a className={style.button}>Смотреть</a>*/}
-                <button className={style.button}>Смотреть</button>
+            <div className={style.photo}>
+                <img
+                    src={props.logo}
+                    alt={`project ${props.logo}`}
+                    className={style.photo}
+                />
+                <button className={style.button}><a href={props.linkToGHPage}>Look</a></button>
             </div>
             <div className={style.workInfo}>
                 <h3 className={style.title}>{props.title}</h3>
